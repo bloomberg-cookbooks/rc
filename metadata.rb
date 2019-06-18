b429236365d9705236c93ef1d7b18dce98f78803
Joshua Justice <jjustice6@bloomberg.net>
2019-06-18 12:08:31 -0400

    Fix the URLs in metadata for the new cookbook location.

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,8 @@ license 'Apache-2.0'
 description 'Library cookbook which provides a resource for writing runtime configuration files.'
 long_description 'Library cookbook which provides a resource for writing runtime configuration files.'
 version '2.0.0'
-source_url 'https://github.com/bloomberg/rc-cookbook' if defined?(:source_url)
-issues_url 'https://github.com/bloomberg/rc-cookbook/issues' if defined?(:source_url)
+source_url 'https://github.com/bloomberg-cookbooks/rc' if defined?(:source_url)
+issues_url 'https://github.com/bloomberg-cookbooks/rc/issues' if defined?(:source_url)
 chef_version '>= 12.5.0'
 
 supports 'ubuntu'
